baa16487d105f521819e29e78b81df7a48671c29
a good neighbor <shared@complicated.com>
2018-12-23 00:38:26 +0000

    remove unused stuff

--- server/server.py
+++ server/server.py
@@ -56,68 +56,16 @@ def metaselfdeploy():
                       +'upload'+": Failed, stderr: "+stderr_data)
         return flask.Response('The Use Case Failed, Brah', status=500)
     return 'deployed!'
-		
-@app.route('/status/<path:path>',methods = ['POST'])
-def audiomessageapi_update():
-    resp = common_route_handler(request, 'update')
-    return resp
 
-@app.route('/')
-def hello_world():
-    return send_from_directory('/opt/webroot/', 'index.html')
-
+@app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello_worldz(path):
+    flaskprint('path: ' + path)
+    if path == '':
+      return send_from_directory('/opt/webroot/', 'index.html')
+
     return send_from_directory('/opt/webroot/', path)
 
-# Grabs stuff out of the HTTP Request
-# Executes Use Case, Gives Request Model to it
-def common_route_handler(request, usecase):
-
-    flaskprint('\n\n\n\n\n\nFlask Route Handler:  '+usecase+'\n')
-
-    # Handle a File Upload
-    # Save the Audio Blob to filesystem
-    if 'file' in request.files:
-        f = request.files['file']
-        pathtofile = audiofilespath+secure_filename(f.filename)
-        f.save(pathtofile)
-        flaskprint('[route] Received a File, saved blob to'+pathtofile+'\n')
-    
-    # Request must have a requestmodel
-    if 'requestmodel' not in request.form:
-        flaskprint('[route] AJAX attempt with no requestmodel \n')
-        return flask.Response('No requestmodel', status=500)
-
-    # Validate we get a JSON
-    json_str = request.form['requestmodel'].encode('utf-8')
-    json_dict = json.loads(json_str)
-    flaskprint('[RequestModel, Parsed]: ')
-    flaskprint(json_dict)
-
-    # If there was a File Upload, include the Path in the RequestModel
-    if 'file' in request.files:
-        json_dict['pathtofile'] = pathtofile 
-        json_str = json.dumps(json_dict)
-
-    # Execute Use Case and Pass the JSON to it
-    p = Popen([boundarypath+'/'+usecase], 
-                      stdout=PIPE, stdin=PIPE, stderr=PIPE)
-    stdout_data,stderr_data  = p.communicate(input=json_str+'\n')
-    flaskprint('[UseCase Output] '+stdout_data)
-
-    # Check Exit Code of the boundary
-    if p.returncode != 0:
-        flaskprint("[UseCase Return Code != 0]: /"
-                      +usecase+": Failed, stderr: "+stderr_data)
-        return flask.Response('The Use Case Failed, Brah', status=500)
-        
-
-    # Send response
-    response_text = stdout_data
-    resp = flask.Response(response_text)
-    resp.headers['Access-Control-Allow-Origin'] = '*'
-    return resp
 	
 def flaskprint(stupid):
     print(stupid, file=sys.stderr)
