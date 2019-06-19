a8a6a6346487febe99d66ffb58a03393dce8133a
Steve Anderson <steve@bootladder.com>
2019-06-19 17:55:41 -0400

    readme

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # A Free Jekyll Builder For You!
 
-You don't need a ruby installation on your laptop to do jekyll builds.  Just use this!  
+You don't need a ruby installation on your laptop to do jekyll builds.  Just put this on a server!  
 
 If you have multiple non-technical writers, building Jekyll sites locally is not an option, so you build on the cloud.  You can set up Travis to build your site, you can build on your own server, or you can use **freejekyllbuilder.com**.  (the site is not up at the moment)
 
+# Usage
 **Build your site:**   
 
 First zip it up at the root and note its location.  
@@ -22,5 +23,14 @@ wget freejekyllbuilder.com/download/mygeneratedusername1
 ```
 **Deploy your site:**  
 You know what to do.  Unzip the _site and deploy the contents in your web root!
+  
+**Here's a description of the pipeline I used:**
+
+* Git push --> Webhook to Travis
+* Travis zips the site source and uploads to freejekyllbuilder
+* Travis then hits a webhook on the static site server
+* Static site server downloads from freejekyllbuilder and deploys
 
 
+# Installation
+`docker-compose up` should work. 
--- server/server.py
+++ server/server.py
@@ -10,33 +10,33 @@ from subprocess import Popen, PIPE, STDOUT
 
 app = Flask(__name__)
 CORS(app)
-app.config['MAX_CONTENT_PATH'] = 1024*1024*128
+app.config['MAX_CONTENT_PATH'] = 1024*1024*128 #to accept large zip files
 
 boundarypath = "/opt/boundary/"
-audiofilespath = "/opt/files/"
+filestoragepath = "/opt/files/"
 
+# Receives a zip file containing the site source
+# Save the file, then build the site with jekyll
 @app.route('/upload/<path:path>', methods = ['POST'])
 def upload_file(path):
     flaskprint('\n\nUpload:  ' + path )
     if 'file' not in request.files:
-      return "not in there"
+      return "no file in there"
 
     flaskprint('\n\nrequest.files:  ' + request.files['file'].filename )
-    return "hello"
-    #f = request.files.values()[0]
+
     f = request.files['file']
-    #pathtofile = audiofilespath+secure_filename(f.filename)
-    pathtofile = audiofilespath+path
+    pathtofile = filestoragepath+path
     f.save(pathtofile)
     flaskprint('[route] Received a File, saved blob to'+pathtofile+'\n')
 
-    # Execute Use Case 
-    p = Popen(['sh',boundarypath+'/'+'upload',path], 
+    # Execute Use Case
+    p = Popen(['sh',boundarypath+'/'+'upload',path],
                       stdout=PIPE, stdin=PIPE, stderr=PIPE)
     stdout_data,stderr_data  = p.communicate(input=pathtofile+'\n')
     flaskprint('[UseCase Output] '+stdout_data)
 
-    # Check Exit Code of the boundary
+    # Check Exit Code
     if p.returncode != 0:
         flaskprint("[UseCase Return Code != 0]: /"
                       +'upload'+": Failed, stderr: "+stderr_data)
@@ -44,11 +44,15 @@ def upload_file(path):
 
     return "response!"
 
+# User Downloads the built site here
 @app.route('/download/<path:path>')
 def send_js(path):
-    return send_from_directory(audiofilespath, path+'.zip',
+    return send_from_directory(filestoragepath, path+'.zip',
         as_attachment=True,attachment_filename=path+'.zip')
 
+
+# Assuming the homepage of freejekyllbuilder.com is hosted on the same host,
+# This server.py can conveniently deploy the site to the web root.
 @app.route('/metaselfdeploy',methods = ['POST','GET'])
 def metaselfdeploy():
     p = Popen(['sh',boundarypath+'/'+'metaselfdeploy'], 
@@ -63,6 +67,8 @@ def metaselfdeploy():
         return flask.Response('The Use Case Failed, Brah', status=500)
     return 'deployed!'
 
+# Assuming the homepage of freejekyllbuilder.com is hosted on the same host,
+# Serve the static site
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def hello_worldz(path):
@@ -72,14 +78,16 @@ def hello_worldz(path):
 
     return send_from_directory('/opt/webroot/', path)
 
-	
+# For debugging... print() doesn't print to flask console for some reason...
 def flaskprint(stupid):
     print(stupid, file=sys.stderr)
 
+# Run the flask server.
+# NOTE the commented out SSL stuff.
 if __name__ == '__main__':
-		context = ('cert.crt', 'key.key')
-		app.run(debug = True,host='0.0.0.0', threaded=True, 
-    ssl_context=context, 
+		#context = ('cert.crt', 'key.key')
+		app.run(debug = True,host='0.0.0.0', threaded=True,
+    #ssl_context=context,
     port=9004)
 
 
